fe09e0eaab0d5195b4740e3f04e1129354add202
Tenthsin <55300869+OnTheFifth@users.noreply.github.com>
2022-06-10 21:56:38 -0700

    Gameplay - shared files

---
+++ cfx-server-data/resources/[Main-Power]/MP-Base/shared/shared.lua
@@ -0,0 +1,22 @@
+MP = MP or {}
+MP.Base = MP.Base or {}
+MP.Player = MP.Player or {}
+MP.Players = MP.Players or {}
+MP.APlayer = MP.APlayer or {}
+MP.APlayers = MP.APlayers or {}
+MP.Functions = MP.Functions or {}
+MP.PlayerData = MP.PlayerData or {}
+MP.Commands = MP.Commands or {}
+MP.Admin = MP.Admin or {}
+
+MP.NewCharacter = {
+    Cash = 600,
+    Bank = 5500,
+}
+
+MP.UserGroups = {
+    ['user'] = {label = 'User'},
+    ['mod'] = {label = 'Moderator'},
+    ['admin'] = {label = 'Admin'},
+    ['dev'] = {label = 'Developer'},
+}
