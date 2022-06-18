1d80bf4f5c49923ea50d258418dc75c9c7fdd2d1
Tenthsin <55300869+OnTheFifth@users.noreply.github.com>
2022-06-18 16:23:00 -0700

    database stup for characters and admins.

---
+++ cfx-server-data/resources/[Main-Power]/MP-Base/character/db_char.lua
@@ -0,0 +1,51 @@
+MP.DB = MP.DB or {}
+MP.AdminPlayer = {}
+
+MP.DB.LoadCharacter = function(source, license, identifier, cid)
+    local src = source
+    local PlayerData = {
+        identifier = identifier,
+        license = license,
+        cid = cid,
+        name = GetPlayerName(src),
+        cash = MP.Starting.Cash,
+        bank = MP.Starting.Bank,
+        citizenid = '' .. cid .. '-' .. identifier .. '',
+    }
+
+    MP.Functions.LoadPlayer(source, PlayerData, cid)
+end
+
+MP.DB.doesUserExist = function(identifier, callback)
+    TriggerEvent('MP-Base:server:doesUserExist', identifier, callback)
+end
+
+MP.DB.SavePlayer = function(source, identifier)
+    print('[MP-Base] ' .. GetPlayerName(source) .. ' was saved successfully!' )
+end
+
+RegisterServerEvent('MP-Admin:Setup')
+AddEventHandler('MP-Admin:Setup', function(source, identifier)
+    MP.Admin.Setup(source, identifier)
+end)
+
+MP.Admin.Setup = function(source, identifier)
+    exports['ghmattimysql']:execute('SELECT * FROM ranking WHERE identifier = @identifier', {['@identifier'] = identifier}, function(result)
+        local slefA = {}
+        slefA.Data = {}
+        slefA.Functions = {}
+
+        slefA.Data.identifier = result[1].identifier
+        slefA.Data.usergroup = result[1].usergroup
+
+        slefA.Functions.setGroup = function(group)
+            slefA.Data.usergroup = group
+            MP.Functions.setGroup(slefA, group)
+        end
+
+        MP.APlayers[source] = slefA
+        print(slefA.Data.usergroup)
+        ExecuteCommand('add_principal identifier.' .. result[1].identifier .. " group." .. slefA.Data.usergroup)
+        print('[MP] '..result[1].identifier..', Updated To Group: '..slefA.Data.usergroup..' was successfull!')
+    end)
+end
---
+++ cfx-server-data/resources/[Main-Power]/MP-Base/player/player.lua
@@ -0,0 +1,64 @@
+MP.Player = {}
+
+MP.Player.LoadData = function(source, identifier, cid)
+
+    exports['ghmattimysql']:execute('SELECT * FROM players WHERE identifier = @identifier AND cid = @cid', {['@identifier'] = identifier, ['@cid'] = cid}, function(result)
+        local self = {}
+        self.Data = {}
+        self.Function = {}
+
+        self.Data.PlayerId = source
+        self.Data.identifier = result[1].identifier
+        self.Data.license = result[1].license
+        self.Data.name = result[1].name
+        self.Data.cid = cid
+        self.Data.job = result[1].job
+        self.Data.firstname = result[1].firstname
+        self.Data.lastname = result[1].lastname
+        self.Data.cash = result[1].cash
+        self.Data.bank = result[1].bank
+
+        self.Functions.addCash = function(amount)
+            MP.Functions.addCash(self, amount)
+            self.Data.cash = self.Data.cash + amount
+        end
+
+        self.Functions.GiveCash = function(amount)
+            MP.Functions.GiveCash(self, amount)
+            self.Data.cash = self.Data.cash + amount
+        end
+
+        self.Functions.removeCash = function(amount)
+            MP.Functions.removeCash(self, amount)
+            self.Data.cash = self.Data.cash - amount
+        end
+
+        self.Functions.addBank = function(amount)
+            MP.Functions.addBank(self, amount)
+            self.Data.bank = self.Data.bank + amount
+        end
+
+        self.Functions.GiveBank = function(amount)
+            MP.Functions.GiveBank(self, amount)
+            self.Data.bank = self.Data.bank + amount
+        end
+
+        self.Functions.removeBank = function(amount)
+            MP.Functions.removeBank(self, amount)
+            self.Data.bank = self.Data.bank + amount
+        end
+
+        self.Functions.setCash = function(amount)
+            self.Data.cash =  amount
+            MP.Functions.setCash(self, amount)
+        end
+
+        self.Functions.setBank = function(amount)
+            self.Data.bank =  amount
+            MP.Functions.setBank(self, amount)
+        end
+
+        MP.Players[source] = self
+    end)
+    
+end
