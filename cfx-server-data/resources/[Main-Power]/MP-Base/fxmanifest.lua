622c252b3f92c30561d38d1ac28bc4b2a6fdb310
Tenthsin <55300869+OnTheFifth@users.noreply.github.com>
2022-07-09 15:47:31 -0700

    Update fxmanifest.lua

--- cfx-server-data/resources/[Main-Power]/MP-Base/fxmanifest.lua
+++ cfx-server-data/resources/[Main-Power]/MP-Base/fxmanifest.lua
@@ -5,40 +5,32 @@ game 'gta5'
 
 dependency 'ghmattimysql'
 
-author 'JSP n StayFly'
+author 'JSP'
 
-ui_page 'html/ui.html'
-
-dependencies {
-	"JC-Logs"
-}
-
+ui_page 'html/index.html'
 
 client_scripts{
-	"shared/sh.lua",
-	"player/player.lua",
-	"events/cl_events.lua",
-	"gameplay/gameplay.lua",
-	"core/cl_core.lua",
-	"character/cl_char.lua",
+    "shared/shared.lua",
+    "events/cl_events.lua",
+    "gameplay/gameplay.lua",
+    "core/cl_core.lua",
+    "character/cl_char.lua",
 }
 
 server_scripts{
-	"shared/sh.lua",
-	"events/sv_events.lua",
-	"character/db_char.lua",
-	"character/sv_char.lua",
-	"core/sv_core.lua",
-	"commands/sv_commands.lua",
-	"player/player.lua",
-
+    "shared/shared.lua",
+    "events/sv_events.lua",
+    "character/db_char.lua",
+    "character/sv_char.lua",
+    "core/sv_core.lua",
+    "commands/sv_commands.lua",
+    "player/player.lua",
 }
-
 
 files{
 	"html/img/background.png",
 	"html/img/logo.png",
 	"html/main.css",
-	"html/ui.html",
+	"html/index.html",
 	"html/app.js",
 }
