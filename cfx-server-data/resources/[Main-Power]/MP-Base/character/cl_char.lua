abc3169170dff9153b6c2c940485de3b22576159
Tenthsin <55300869+OnTheFifth@users.noreply.github.com>
2022-07-09 15:41:04 -0700

    Fixed Small Mistypes in CL_Char

--- cfx-server-data/resources/[Main-Power]/MP-Base/character/cl_char.lua
+++ cfx-server-data/resources/[Main-Power]/MP-Base/character/cl_char.lua
@@ -11,9 +11,9 @@ Citizen.CreateThread(function()
         if NetworkIsSessionStarted() then 
             TriggerServerEvent('MP-Base:Char:Joined')
             TriggerEvent('MP-Base:Char:StartCamera')
-            TriggerEvent('MP-ui:client:CloseCharUI')
-            TriggerEvent('MP-Base:PlayerLogin')
-            selectedChar(true)
+            -- TriggerEvent('MP-ui:client:CloseCharUI')
+            -- TriggerEvent('MP-Base:PlayerLogin')
+            SelectChar(true)
             return
         end
     end
@@ -21,7 +21,7 @@ end)
 
 RegisterNetEvent('MP-Base:Char:Selecting')
 AddEventHandler('MP-Base:Char:Selecting', function()
-    selectedChar(true)
+    SelectChar(true)
 end)
 
 GetCID = function(source, cb)
@@ -30,20 +30,20 @@ GetCID = function(source, cb)
 end
 
 RegisterNUICallback('createCharacter', function(data)
-    local CharData = data.CharData
-    for theData, value in pains(CharData) do
-        if theData == 'firstname' or theData == 'lastname' then 
-            reason = verifyName(value)
-            print(reason) -- make sure this prints out correctly comment// remove later
+    local charData = data.charData
+    for theData, value in pairs(charData) do
+		if theData == "firstname" or theData == "lastname" then
+			reason = verifyName(value)
+            print(reason)
 
-            if reason ~= '' then
-                break
+			if reason ~= "" then
+				break
             end
-        end
-    end
-
-    if reason == '' then 
-        TriggerServerEvent('MP-Base:server:createCharacter', CharData)
+		end
+	end
+  
+    if reason == "" then
+        TriggerServerEvent('MP-Base:server:createCharacter', charData) -- creates character and makes data
     end
 end)
 
@@ -90,46 +90,46 @@ function verifyName(name)
 end
 
 RegisterNUICallback('deleteCharacter', function(data)
-    local CharData = data 
-    TriggerServerEvent('MP-Base:deleteChar', CharData)
+    local charData = data 
+    TriggerServerEvent('MP-Base:deleteChar', charData)
 end)
 
-RegisterNetEvent('MP-Base:Char:setupCharacter')
-AddEventHandler('MP-Base:Char:setupCharacter', function()
-    MP.Functions.TriggerServerCallback('MP-Base:GetChar', function(data)
-        SendNUIMessage({type = 'setupCharacters', characters = data})
+RegisterNetEvent('MP-Base:Char:setupCharacters')
+AddEventHandler('MP-Base:Char:setupCharacters', function()
+    MP.Functions.TriggerServerCallback('MP-Base:getChar', function(data) -- Gets all data from character
+        SendNUIMessage({type = "setupCharacters", characters = data})
     end)
 end)
 
-RegisterNUICallback('selectCharacters', function(data)
+RegisterNUICallback('selectCharacter', function(data)
     local cid = tonumber(data.cid)
-    selectedChar(false)
+    SelectChar(false)
     TriggerServerEvent('MP-Base:Char:ServerSelect', cid)
-    TriggerEvent('MP-Spawn:openMenu')
+    -- TriggerEvent('MP-Spawn:openMenu') add menu to spawn
     SetTimecycleModifier('default')
     SetCamActive(cam, false)
     DestroyCam(cam, false)
 end)
 
 RegisterNUICallback('CloseChar', function()
-    selectedChar(false)
+    SelectChar(false)
 end)
 
-function selectedChar(value)
+function SelectChar(value)
     SetNuiFocus(value, value)
     SendNUIMessage({
         type = 'charSelect',
         status = value
     })
-    selectingChar = false
+    selectingChar = value
 end
 
-RegisterNetEvent('MP-Base:Char:StartCam')
-AddEventHandler('MP-Base:Char:StartCam', function()
+RegisterNetEvent('MP-Base:Char:StartCamera')
+AddEventHandler('MP-Base:Char:StartCamera', function()
     DoScreenFadeIn(10)
     SetTimecycleModifier('hud_def_blur')
     SetTimecycleModifierStrength(1.0)
-    FreezeEntityPosition(GetPlayedPed(-1), true)
+    FreezeEntityPosition(GetPlayerPed(-1), true)
     cam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", -358.56, -981.96, 286.25, 320.00, 0.00, -50.00, 90.00, false, 0)
     SetCamActive(cam, true)
     RenderScriptCams(true,false,1,true,true)
